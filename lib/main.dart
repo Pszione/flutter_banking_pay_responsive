55dc486595502c3f06d865a13b12a7b17c015c4a
Pszione <oused.games@gmail.com>
2021-09-08 16:33:35 -0300

    Banking Pay - Material Theming part II | get Provider package

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  /*const*/ MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -16,49 +16,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Banking Pay',
       debugShowCheckedModeBanner: false,
-      theme: appThemeData,
+      themeMode: ThemeMode.system,
+      theme: AppThemes.lightThemeData,
+      darkTheme: AppThemes.darkThemeData,
       home: SetupScreen(),
     );
   }
+}
 
-  final ThemeData appThemeData = ThemeData(
+class AppThemes {
+  static ColorScheme appColorScheme = const ColorScheme.light(
+    primary: kPrimaryColor,
+    secondary: kSecondaryColor,
+    //primaryVariant:
+    //secondaryVariant: ,
+    surface: kWhiteColor,
+    background: kWhiteColor,
+  );
+  static ColorScheme appDarkColorScheme = const ColorScheme.dark(
+    primary: kPrimaryColor,
+    secondary: kPrimaryColor,
+    primaryVariant: kPrimaryColor,
+    secondaryVariant: kPrimaryColor,
+    surface: kDarkBackgroundColor,
+    background: kDarkBackgroundColor,
+  );
+
+  static final ThemeData lightThemeData = ThemeData(
     appBarTheme: const AppBarTheme(
       backgroundColor: kWhiteColor,
       elevation: 0,
     ),
-    colorScheme: appColorScheme,
+    colorScheme: AppThemes.appColorScheme,
 
-    scaffoldBackgroundColor: kBackgroundColor,
+    scaffoldBackgroundColor: kBackgroundColor, // kWhiteColor
     // primarySwatch: createMaterialColor(kComplementaryColor),
     fontFamily: GoogleFonts.poppins().fontFamily,
   );
-}
 
-ColorScheme appColorScheme = const ColorScheme.light(
-  primary: kPrimaryColor,
-  secondary: kSecondaryColor,
-  //primaryVariant:
-  //secondaryVariant: ,
-  surface: kWhiteColor,
-  background: kWhiteColor,
-);
+  static final ThemeData darkThemeData = ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: kDarkBackgroundColor,
+      elevation: 0,
+    ),
+    colorScheme: AppThemes.appDarkColorScheme,
 
-MaterialColor createMaterialColor(Color color) {
-  List strengths = <double>[.05];
-  final swatch = <int, Color>{};
-  final int r = color.red, g = color.green, b = color.blue;
+    scaffoldBackgroundColor: kDarkBackgroundColor,
+    // primarySwatch: createMaterialColor(kComplementaryColor),
+    fontFamily: GoogleFonts.poppins().fontFamily,
+  );
 
-  for (int i = 1; i < 10; i++) {
-    strengths.add(0.1 * i);
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    final swatch = <int, Color>{};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
   }
-  for (var strength in strengths) {
-    final double ds = 0.5 - strength;
-    swatch[(strength * 1000).round()] = Color.fromRGBO(
-      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-      1,
-    );
-  }
-  return MaterialColor(color.value, swatch);
 }
---
+++ lib/theme_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.dark;
+
+  bool get isDarkMode => themeMode == ThemeMode.dark;
+}
