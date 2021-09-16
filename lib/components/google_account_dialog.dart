5a0816722f4c77c9834279dff544cb33efb8c492
Pszione <oused.games@gmail.com>
2021-09-16 12:12:55 -0300

    Banking Pay - Flutter update | GoogleAccountDialog minor ajusts

--- lib/components/google_account_dialog.dart
+++ lib/components/google_account_dialog.dart
@@ -17,87 +17,94 @@ class GoogleAccountDialog {
       barrierDismissible: true, // click outside to dismiss
       barrierColor: Colors.black.withOpacity(0.70),
       builder: (BuildContext context) => AlertDialog(
-        title: buildGoogleHeader(context),
         elevation: 0,
         backgroundColor: Theme.of(context).colorScheme.background,
         shape: RoundedRectangleBorder(borderRadius: kDefaultBorderRadius),
-        titlePadding: const EdgeInsets.all(kHalfPadding),
+        titlePadding: const EdgeInsets.fromLTRB(
+            kDefaultPadding, kHalfPadding, kDefaultPadding, 0),
         contentPadding: EdgeInsets.zero,
-        insetPadding: const EdgeInsets.symmetric(
-            horizontal: kDefaultPadding, vertical: kHugePadding),
+        insetPadding: const EdgeInsets.only(
+            left: kDefaultPadding, right: kDefaultPadding),
+        title: buildGoogleHeader(context),
         // TODO: scroll is not working
-        content: SingleChildScrollView(
-          child: SizedBox(
-            width: 400, // MediaQuery.of(context).size.width * 0.9,
-            height: 420,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                buildAccountItem(
-                  context,
-                  'Pedro Santos',
-                  'pedrinho554@gmail.com',
-                  'assets/images/35244548_pedro_santos.png',
-                  kSmallIconSize,
-                  () => Navigator.pop(context),
-                ),
+        content: SizedBox(
+          width: 400, // MediaQuery.of(context).size.width * 0.9,
+          height: 420,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              buildAccountItem(
+                context,
+                'Pedro Santos',
+                'pedrinho554@gmail.com',
+                'assets/images/35244548_pedro_santos.png',
+                kSmallIconSize,
+                () => Navigator.pop(context),
+              ),
+              if (MediaQuery.of(context).orientation == Orientation.portrait)
                 buildManageAccountButton(context),
-                const SizedBox(height: kHalfPadding),
-                kDivider,
-                Column(
-                  children: [
-                    buildAccountItem(
-                      context,
-                      'Oused Games',
-                      'oused.games@gmail.com',
-                      'assets/icons/logo_master_card.png',
-                      kSmallIconSize02,
-                      () {},
-                    ),
-                    buildAccountItem(
-                      context,
-                      'Eu Acredito Na Humanidade',
-                      'euacreditoblog@gmail.com',
-                      'assets/images/_earth_TEST07B.jpg',
-                      kSmallIconSize02,
-                      () {},
-                    ),
-                  ],
+              const SizedBox(height: kHalfPadding),
+              kDivider,
+              SizedBox(
+                height: 165,
+                child: SingleChildScrollView(
+                  physics: const ClampingScrollPhysics(),
+                  child: Column(
+                    children: [
+                      buildAccountItem(
+                        context,
+                        'Oused Games',
+                        'oused.games@gmail.com',
+                        'assets/icons/logo_master_card.png',
+                        kSmallIconSize02,
+                        () {},
+                      ),
+                      buildAccountItem(
+                        context,
+                        'Eu Acredito Na Humanidade',
+                        'euacreditoblog@gmail.com',
+                        'assets/images/_earth_TEST07B.jpg',
+                        kSmallIconSize02,
+                        () {},
+                      ),
+                    ],
+                  ),
                 ),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    BuildGoogleListButton(
-                        label: 'Add another account',
-                        icon: Icons.person_add,
-                        onPress: () => Navigator.pop(context)),
-                    kDivider,
-                    // TODO: add link
-                    BuildGoogleListButton(
-                      label: 'Settings',
-                      icon: Icons.settings_rounded,
-                      onPress: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  const SettingsScreen())),
-                    ),
-                    // TODO: add FAQ link
-                    BuildGoogleListButton(
-                        label: 'Help',
-                        icon: Icons.help_outline_sharp,
-                        onPress: () => Navigator.pop(context)),
-                  ],
-                ),
-                kDivider
-              ],
-            ),
+              ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  BuildGoogleListButton(
+                      label: 'Add another account',
+                      icon: Icons.person_add,
+                      onPress: () => Navigator.pop(context)),
+                  kDivider,
+                  // TODO: add link
+                  BuildGoogleListButton(
+                    label: 'Settings',
+                    icon: Icons.settings_rounded,
+                    onPress: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const SettingsScreen())),
+                  ),
+                  // TODO: add FAQ link
+                  BuildGoogleListButton(
+                      label: 'Help',
+                      icon: Icons.help_outline_sharp,
+                      onPress: () => Navigator.pop(context)),
+                ],
+              ),
+              kDivider
+            ],
           ),
         ),
-        actionsAlignment: MainAxisAlignment.center,
         actionsPadding: EdgeInsets.zero,
+        actionsAlignment: MainAxisAlignment.center,
         actions: buildPolicyAndTermsButtons(context),
+        buttonPadding: EdgeInsets.zero,
       ),
     );
   }
@@ -106,8 +113,7 @@ class GoogleAccountDialog {
     return Stack(
       children: [
         IconButton(
-          // padding: const EdgeInsets.only(
-          //     left: kHalfPadding / 2, top: kHalfPadding / 2),
+          padding: const EdgeInsets.symmetric(vertical: 8),
           alignment: Alignment.topLeft,
           icon: const Icon(Icons.close_rounded),
           onPressed: () => Navigator.pop(context, 'X'),
@@ -193,6 +199,7 @@ class GoogleAccountDialog {
         onPressed: () => {},
         child: Text(
           'Privacy Policy',
+          maxLines: 1,
           style: TextStyle(
               fontSize: 12, color: Theme.of(context).colorScheme.onBackground),
         ),
@@ -202,10 +209,11 @@ class GoogleAccountDialog {
         onPressed: () => {},
         child: Text(
           'Terms of Service',
+          maxLines: 1,
           style: TextStyle(
               fontSize: 12, color: Theme.of(context).colorScheme.onBackground),
         ),
-      ),
+      )
     ];
   }
 }
