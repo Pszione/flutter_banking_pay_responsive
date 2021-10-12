2a0a32eb6a39d843bb356e94e523352025e865b2
Pszione <oused.games@gmail.com>
2021-10-11 23:18:51 -0300

    Banking Pay - Created HomeScreen categories card

---
+++ lib/screens/homeScreen/categories_cards.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../constants.dart';
+
+class CategoriesSection extends StatelessWidget {
+  const CategoriesSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // final List<Map<String, String>> categories = [
+    //   <String, String>{
+    //     'icon': FontAwesomeIcons.fileInvoice,
+    //     'text': 'Bills Resume'
+    //   },
+    //   <String, String>{
+    //     'icon': FontAwesomeIcons.shoppingBag,
+    //     'text': 'Sales Store'
+    //   },
+    //   <String, String>{
+    //     'icon': FontAwesomeIcons.solidCommentDots,
+    //     'text': 'Support Chat'
+    //   },
+    //   <String, String>{'icon': Icons.more_horiz, 'text': 'More'},
+    // ];
+
+    const spacing = SizedBox(width: kHalfPadding * 1.2, height: kHalfPadding);
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const ClampingScrollPhysics(),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CategoryCard(
+            label: 'Transactions',
+            icon: Icons.read_more_rounded,
+            size: kMediumIconSize * 1.25,
+            onPress: () => {},
+          ),
+          spacing,
+          CategoryCard(
+            label: 'Bills Resume',
+            icon: FontAwesomeIcons.fileInvoice,
+            onPress: () => {},
+          ),
+          spacing,
+          CategoryCard(
+            label: 'Sales Store',
+            icon: FontAwesomeIcons.shoppingBag,
+            onPress: () => {},
+          ),
+
+          spacing,
+          CategoryCard(
+            label: 'Support Chat',
+            icon: FontAwesomeIcons.solidCommentDots,
+            onPress: () => {},
+          ),
+          spacing,
+          CategoryCard(
+            label: 'more',
+            icon: Icons.more_horiz,
+            onPress: () => {},
+          ),
+          //
+          // ...List<CategoryCard>.generate(
+          //   categories.length,
+          //   (int index) => CategoryCard(
+          //     icon: categories[index]['icon'],
+          //     label: categories[index]['text'],
+          //     onPress: () => {},
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+    required this.icon,
+    required this.label,
+    this.size = kMediumIconSize,
+    required this.onPress,
+  }) : super(key: key);
+
+  final String label;
+  final IconData icon;
+  final double size;
+  final GestureTapCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).colorScheme.secondary,
+      //focusColor: Colors.transparent,
+      //highlightColor: Colors.transparent,
+      borderRadius: kHugeBorderRadius,
+      radius: kInkWellMediumRadius / 1.5,
+      onTap: onPress,
+      child: SizedBox(
+        width: 55,
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                padding: const EdgeInsets.all(kHalfPadding),
+                decoration: BoxDecoration(
+                    color: kGrayColor,
+                    borderRadius:
+                        BorderRadius.circular(kDefaultBorderRadiusAsDouble),
+                    boxShadow: [kBoxDownShadowSubtle]),
+                child: Icon(
+                  icon,
+                  color: Colors.black,
+                  size: size,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              label,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  height: kTextMediumHeightSpacing * 0.9),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/homeScreen/home_screen.dart
+++ lib/screens/homeScreen/home_screen.dart
@@ -3,10 +3,12 @@ import 'package:flutter_banking_pay_responsive/components/app_bar_complete.dart'
 import 'package:flutter_banking_pay_responsive/components/app_floating_button_with_icon_and_text.dart';
 import 'package:flutter_banking_pay_responsive/screens/homeScreen/recent_transactions_section.dart';
 import 'package:flutter_banking_pay_responsive/screens/homeScreen/user_cards_section.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../constant_text_styles.dart';
 import '../../constants.dart';
 import '../../snackbar_errors.dart';
+import 'categories_cards.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -41,50 +43,14 @@ class HomeScreen extends StatelessWidget {
               children: <Widget>[
                 const UserCardsSection(),
                 const SizedBox(height: kDefaultPadding),
+                const CategoriesSection(),
+                const SizedBox(height: kDefaultPadding),
                 const RecentTransactionsSection(),
                 const SizedBox(height: kDefaultPadding),
-                //const TransferMoneySection(),
                 const SizedBox(height: 160),
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class TransferMoneySection extends StatelessWidget {
-  const TransferMoneySection({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        iconSize: 160,
-        splashRadius: kInkWellMediumRadius,
-        onPressed: () =>
-            AppSnackBarErrors.showSnackBarFeatureUnavailable(context),
-        icon: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: kMediumIconSize,
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              child: Icon(
-                Icons.ios_share,
-                size: kHugeIconSize,
-                color: Theme.of(context).primaryColorDark,
-              ),
-            ),
-            const SizedBox(height: kHalfPadding),
-            Text(
-              'Transfer Money',
-              style: AppTextStyles.kListTileTitle,
-            ),
-          ],
         ),
       ),
     );
