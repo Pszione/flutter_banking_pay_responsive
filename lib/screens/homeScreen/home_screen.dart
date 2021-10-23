96603f7829ea2b48ec84a87c1e7059cffc28a906
Pszione <oused.games@gmail.com>
2021-10-22 23:58:57 -0300

    Banking Pay - FAB ScrollNotificationListener part I | Make HomeScreen stateful

--- lib/screens/homeScreen/home_screen.dart
+++ lib/screens/homeScreen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_banking_pay_responsive/components/app_bar_complete.dart';
 import 'package:flutter_banking_pay_responsive/components/app_floating_button_with_icon_and_text.dart';
 import 'package:flutter_banking_pay_responsive/screens/homeScreen/recent_transactions_section.dart';
@@ -8,8 +9,15 @@ import '../../constants.dart';
 import '../../snackbar_errors.dart';
 import 'categories_cards.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isFloatingButtonVisible = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,36 +28,50 @@ class HomeScreen extends StatelessWidget {
         hasDarkThemeToggle: true,
       ),
       // Cards
-      floatingActionButton: AppFloatingButtonIconAndText(
-        label: null,
-        tooltip: 'Options',
-        icon: Icons.read_more_rounded,
-        onPressed: () =>
-            AppSnackBarErrors.showSnackBarFeatureUnavailable(context),
-      ),
+      floatingActionButton: _isFloatingButtonVisible
+          ? AppFloatingButtonIconAndText(
+              label: null,
+              tooltip: 'Options',
+              icon: Icons.read_more_rounded,
+              onPressed: () =>
+                  AppSnackBarErrors.showSnackBarFeatureUnavailable(context),
+            )
+          : null,
       floatingActionButtonLocation: kFloatingButtonLocationFixed(context),
-      body: SingleChildScrollView(
-        physics: const ClampingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(kDefaultPadding),
-          // Un focus keyboard/textfield
-          child: GestureDetector(
-            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-            behavior: HitTestBehavior.translucent,
-            excludeFromSemantics: true,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Semantics(
-                  child: const UserCardsSection(),
-                ),
-                const SizedBox(height: kDefaultPadding),
-                Semantics(child: const CategoriesSection()),
-                const SizedBox(height: kDefaultPadding),
-                Semantics(child: const RecentTransactionsSection()),
-                const SizedBox(height: kDefaultPadding),
-                const SizedBox(height: 160),
-              ],
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          if (notification.direction == ScrollDirection.forward) {
+            if (!_isFloatingButtonVisible)
+              setState(() => _isFloatingButtonVisible = true);
+          } else if (notification.direction == ScrollDirection.reverse) {
+            if (_isFloatingButtonVisible)
+              setState(() => _isFloatingButtonVisible = false);
+          }
+          return true;
+        },
+        child: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            // Un focus keyboard/textfield
+            child: GestureDetector(
+              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+              behavior: HitTestBehavior.translucent,
+              excludeFromSemantics: true,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Semantics(
+                    child: const UserCardsSection(),
+                  ),
+                  const SizedBox(height: kDefaultPadding),
+                  Semantics(child: const CategoriesSection()),
+                  const SizedBox(height: kDefaultPadding),
+                  Semantics(child: const RecentTransactionsSection()),
+                  const SizedBox(height: kDefaultPadding),
+                  const SizedBox(height: 160),
+                ],
+              ),
             ),
           ),
         ),
