5d951841d812058a510a8b0be13fa4683778a6fc
Pszione <oused.games@gmail.com>
2021-12-20 22:58:49 -0300

    Refactor - Create class for AndroidQuickActions shortcuts

--- lib/screens/setupScreen/setup_screen.dart
+++ lib/screens/setupScreen/setup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_banking_pay_responsive/constant_text_styles.dart';
 import 'package:flutter_banking_pay_responsive/constants.dart';
+import 'package:flutter_banking_pay_responsive/core/android_quick_actions_shotcuts.dart';
 import 'package:flutter_banking_pay_responsive/data_providers.dart';
 import 'package:flutter_banking_pay_responsive/generated/l10n.dart';
 import 'package:flutter_banking_pay_responsive/main.dart';
@@ -11,7 +12,6 @@ import 'package:flutter_banking_pay_responsive/screens/homeScreen/home_screen.da
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:quick_actions/quick_actions.dart';
 
 // ignore_for_file: unused_local_variable, unused_field_variable
 class SetupScreen extends StatefulWidget with ChangeNotifier {
@@ -19,8 +19,6 @@ class SetupScreen extends StatefulWidget with ChangeNotifier {
 
   final keyScreen = GlobalKey<SetupScreenState>();
   late ValueKey<SetupScreenState> keyValueScreen;
-
-  final quickActionsList = const QuickActions();
 
   @override
   State<SetupScreen> createState() => SetupScreenState();
@@ -45,61 +43,8 @@ class SetupScreenState extends State<SetupScreen> {
 
     widget.keyValueScreen = ValueKey(this);
 
-    initializeQuickActionsDelayed();
-  }
-
-  void initializeQuickActionsDelayed() async {
-    await Future.delayed(const Duration(milliseconds: 200));
-    BuildContext _context = widget.keyScreen.currentContext!; // GlobalKey
-
-    // if (Platform.isAndroid || Platform.isIOS || Platform.isMacOS) { // dart.io
-    if (!WebProvider.isWebPlatform) {
-      widget.quickActionsList.setShortcutItems([
-        ShortcutItem(
-            type: QuickActionState.search.toString(),
-            // localizedTitle: 'Search',
-            localizedTitle: S.of(_context).quickActions_first_title,
-            icon: 'quick_search'),
-        ShortcutItem(
-            type: QuickActionState.transactionsOptions.toString(),
-            // localizedTitle: 'New Transaction',
-            localizedTitle: S.of(_context).quickActions_second_title,
-            icon: 'quick_read_more'),
-        ShortcutItem(
-            type: QuickActionState.activity.toString(),
-            // localizedTitle: 'Recent Activities',
-            localizedTitle: S.of(_context).quickActions_third_title,
-            icon: 'quick_chart_outline'),
-        // PS: iOS icons not setup in Runner/Xcode
-      ]);
-    }
-
-    await Future.delayed(const Duration(milliseconds: 200));
-
-    /// Should run only on Android and iOS | macOS?
-    if (!WebProvider.isWebPlatform) {
-      widget.quickActionsList.initialize((String type) {
-        if (type == QuickActionState.search.toString()) {
-          changeSelectedMenuByState(MenuState.home);
-          Provider.of<HomeScreen>(_context, listen: false)
-              .keyValueScreen
-              .value
-              .openCloseStateSearch
-              .value = true;
-          print("Should've open search bar");
-        } else if (type == QuickActionState.transactionsOptions.toString()) {
-          changeSelectedMenuByState(MenuState.home);
-          Provider.of<HomeScreen>(_context, listen: false)
-              .keyValueScreen
-              .value
-              .openFAB_quickAction();
-          print("Should've open FAB");
-        } else if (type == QuickActionState.activity.toString()) {
-          changeSelectedMenuByState(MenuState.insights);
-          print("Should've changed to ActivityInsightsScreen");
-        }
-      });
-    }
+    // BuildContext _context = widget.keyScreen.currentContext!;
+    // QuickActionsCustom().initializeDelayed(_context);
   }
 
   @override
