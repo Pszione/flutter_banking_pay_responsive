f104bb0c73ed5ceb8d58be315b994feb58d286b7
Pszione <oused.games@gmail.com>
2021-12-30 00:50:02 -0300

    Refactor - CardScreen controller part I | get_it dependency injection

--- lib/presentation/controllers/card_screen_controller.dart
+++ lib/presentation/controllers/card_screen_controller.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_banking_pay_responsive/layers/domain/card.dart';
 
-class CardScreenController {}
+class CardScreenController {
+  List<CardModel> cards = [];
+
+  CardScreenController() {
+    cards = myCards;
+  }
+
+  Color getCardColorNullSafety(CardModel card, {double opacity = 1.0}) =>
+      CardModel.getCardColorNullSafety(card: card, opacity: opacity);
+}
--- lib/presentation/ui/screens/cardScreen/card_screen.dart
+++ lib/presentation/ui/screens/cardScreen/card_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_banking_pay_responsive/core/route_controller.dart';
+import 'package:flutter_banking_pay_responsive/presentation/controllers/card_screen_controller.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/app_bar_complete.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/app_floating_button_with_icon_and_text.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/app_sliding_bottom_sheet.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/card_add_sliding_sheet.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/card_overview_sliding_sheet.dart';
-import 'package:flutter_banking_pay_responsive/layers/domain/card.dart';
 import 'package:flutter_banking_pay_responsive/presentation/ui/designSystem/constants.dart';
 import 'package:flutter_banking_pay_responsive/presentation/controllers/data_providers.dart';
 import 'package:flutter_banking_pay_responsive/generated/l10n.dart';
@@ -13,12 +13,13 @@ import 'package:flutter_banking_pay_responsive/main.dart';
 import 'package:flutter_banking_pay_responsive/presentation/ui/designSystem/responsive.dart';
 import 'package:flutter_banking_pay_responsive/presentation/ui/screens/homeScreen/home_screen.dart';
 import 'package:flutter_banking_pay_responsive/presentation/widgets/card_widget.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
 class CardScreen extends StatefulWidget with ChangeNotifier {
   CardScreen({Key? key}) : super(key: key);
 
-  final keyScreen = GlobalKey<CardScreenState>();
+  final GlobalKey<CardScreenState> keyScreen = GlobalKey<CardScreenState>();
   late ValueKey<CardScreenState> keyValueScreen;
 
   @override
@@ -26,11 +27,21 @@ class CardScreen extends StatefulWidget with ChangeNotifier {
 }
 
 class CardScreenState extends State<CardScreen> {
+  var controller = GetIt.I<CardScreenController>();
+
+  Axis _currentScrollAxis = Axis.vertical;
+  bool _isAxisVertical = true;
+
   @override
   void initState() {
     super.initState();
 
     widget.keyValueScreen = ValueKey(this);
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      MyApp.changeWebAppTabName(
+          label: S.of(context).homeScreen_second_tabBarTitle);
+    });
   }
 
   @override
@@ -41,12 +52,10 @@ class CardScreenState extends State<CardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final _currentScrollAxis = Responsive.isMobileLarge(context)
+    _currentScrollAxis = Responsive.isMobileLarge(context)
         ? Axis.vertical
         : Axis.vertical; // TODO Axis.horizontal;
-    final _isAxisVertical = _currentScrollAxis == Axis.vertical;
-    MyApp.changeWebAppTabName(
-        label: S.of(context).homeScreen_second_tabBarTitle);
+    _isAxisVertical = _currentScrollAxis == Axis.vertical;
 
     return Scaffold(
       key: widget.keyScreen,
@@ -63,12 +72,7 @@ class CardScreenState extends State<CardScreen> {
         label: S.of(context).cardScreen_fab_title,
         tooltip: S.of(context).cardScreen_TOOLTIP_fab_options,
         icon: Icons.add,
-        onPressed: () => CardAddSlidingSheet().showOptionsSlidingSheet(context),
-        // onPressed: () => const AppAlertDialog(
-        //   title: 'Add Card',
-        //   contentText: null,
-        // ).showAlertDialogDismissible(context),
-        // AppSnackBarErrors.showSnackBarFeatureUnavailable(context),
+        onPressed: () => handleOnPressFab(context),
       ),
       floatingActionButtonLocation: kFloatingButtonLocationAdaptive(context),
       bottomNavigationBar:
@@ -97,9 +101,9 @@ class CardScreenState extends State<CardScreen> {
                         width: kHalfPadding, height: kDefaultPadding);
                   },
                   shrinkWrap: true,
-                  itemCount: myCards.length + 1,
+                  itemCount: controller.cards.length + 1,
                   itemBuilder: (_, index) {
-                    if (index == myCards.length) {
+                    if (index == controller.cards.length) {
                       // because of +1 in length I have to return something
                       return !_isAxisVertical
                           ? CardWidget.defaultDimension
@@ -108,21 +112,14 @@ class CardScreenState extends State<CardScreen> {
                     return ResponsiveWidthConstrained(
                       child: Center(
                         child: CardWidget(
-                          // TODO: implement controller
-                          card: myCards[index],
+                          card: controller.cards[index],
                           index: index,
-                          onPress: () => AppSlidingBottomSheet(
-                            context: context,
-                            initialSnap: _isAxisVertical ? 0.5 : 1.0,
-                            headerColor: CardModel.getCardColorNullSafety(
-                                card: myCards[index], opacity: 0.85),
-                            bodyWidget:
-                                CardOverviewSlidingSheet(card: myCards[index]),
-                          ).showStyledSheet(),
+                          onPress: () => handleOnPressCard(context, index),
                         ),
                       ),
                     );
                   },
+                  // TODO: violation of responsibilities
                   padding: EdgeInsets.only(
                     left: HomeScreen.desiredPadding.left,
                     right: HomeScreen.desiredPadding.right,
@@ -143,5 +140,24 @@ class CardScreenState extends State<CardScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> handleOnPressFab(BuildContext context) async {
+    await CardAddSlidingSheet().showOptionsSlidingSheet(context);
+    // onPressed: () => const AppAlertDialog(
+    //   title: 'Add Card',
+    //   contentText: null,
+    // ).showAlertDialogDismissible(context),
+    // AppSnackBarErrors.showSnackBarFeatureUnavailable(context),
+  }
+
+  Future<void> handleOnPressCard(BuildContext context, int index) {
+    return AppSlidingBottomSheet(
+      context: context,
+      initialSnap: _isAxisVertical ? 0.5 : 1.0,
+      headerColor: controller.getCardColorNullSafety(controller.cards[index],
+          opacity: 0.85),
+      bodyWidget: CardOverviewSlidingSheet(card: controller.cards[index]),
+    ).showStyledSheet();
   }
 }
